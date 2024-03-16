182492c5c500ada32f48f67cae760084527fd19c
Mantas <scrumshie@gmail.com>
2024-03-16 07:45:11 +0200

    reading frankenstein again

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
 main()

c2073ae17981fead0b6813c9171ec97de76dd92d
Mantas <scrumshie@gmail.com>
2024-03-19 15:59:27 +0200

    completed bookbot project

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    #print(text)
-    print(word_counter(text))
-    letter_counter(text)
+    words = word_counter(text)
+    chars = letter_counter(text)
+    unsorted_dict = dictionarinator(chars)
+    unsorted_dict.sort(reverse=True, key=sort_on)
+    formater(book_path, words, unsorted_dict)
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
@@ -19,6 +22,27 @@ def letter_counter(text):
             letters[char] += 1
         else:
             letters[char] = 1
-    print(letters)
-    pass
+    return letters
+
+#converts dictionary into a list of dictionaries
+def dictionarinator(dict):
+    dict_list = []
+    for char in dict:
+        if char.isalpha() is True:
+            dict_list.append({"char": char, "num":  dict[char]})
+        else:
+            pass
+    return dict_list
+
+def sort_on(dict):
+    return dict["num"]
+
+def formater(document, wc, dict):
+    print(f"--- Begin report of {document} ---")
+    print(f"{wc} words found in the docuemnt \n")
+    for item in dict:
+        print(f"The '{item["char"]}' character was found {item["num"]} times")
+    print("--- End report ---")
+    
+
 main()
